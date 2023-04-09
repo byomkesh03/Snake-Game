a01105bb29005437035d990901973d9200f5da97
Byomkesh Mahato <byomkeshmahato03@gmail.com>
2023-04-09 23:29:51 +0530

    Added music sound

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let inputDir = {x: 0, y: 0};
 const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');

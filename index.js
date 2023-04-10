875a996d46e20fc7634b7046a6661e8b41ee4cef
Byomkesh Mahato <byomkeshmahato03@gmail.com>
2023-04-11 00:30:44 +0530

    Added logic of bump into the wall

--- index.js
+++ index.js
@@ -31,3 +31,9 @@ function isCollide(snake) {
             return true;
         }
     }
+     // If you bump into the wall
+     if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+    return false;

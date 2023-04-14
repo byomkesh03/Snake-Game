c7f29a1e60a0c3b105e770508f06f8da385673e0
Byomkesh Mahato <byomkeshmahato03@gmail.com>
2023-04-14 10:44:07 +0530

    Started main logic

--- index.js
+++ index.js
@@ -98,4 +98,9 @@ function gameEngine(){
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add('food')
     board.appendChild(foodElement);
+
+
 }
+
+
+// Main logic starts here
